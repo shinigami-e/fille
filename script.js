bd135f946d68a5b139aa97dbb5337964ddae8e33
crammer-web <mohamedyamni68@gmail.com>
2026-07-06 22:05:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ node3.appendChild(textnode3);
 node4.appendChild(textnode4)
 document.getElementById("card").appendChild(node1);
 document.getElementById("card").appendChild(node3);
-document.getElementById("card").appendChild(node2);
+//document.getElementById("card").appendChild(node2);
 document.getElementById("card").appendChild(node4);
 }
 if(nam.value=="COUNTRY"){
